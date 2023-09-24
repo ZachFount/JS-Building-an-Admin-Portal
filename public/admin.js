745f90149d828dece8ef9fbdb191736975c4beb1
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-09-24 13:25:02 -0400

    revised

--- public/admin.js
+++ public/admin.js
@@ -1,19 +1,24 @@
-
 async function main() {
 
     let response = await fetch('http://localhost:3001/listBooks')
+
     let books = await response.json()
+
     books.forEach(renderBook)
 }
 
 function renderBook(book) {
     let root = document.querySelector('#root')
+
     let li = document.createElement('li')
     li.textContent = book.title
+
     let quantityInput = document.createElement('input')
     quantityInput.value = book.quantity
+
     let saveButton = document.createElement('button')
     saveButton.textContent = 'Save'
+
     saveButton.addEventListener('click', () => {
         fetch('http://localhost:3001/updateBook', {
             method: 'PATCH',
@@ -28,6 +33,7 @@ function renderBook(book) {
     })
 
     li.append(quantityInput, saveButton)
+
     root.append(li)
 }
 
